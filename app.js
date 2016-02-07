029f3c6194101c912839766ab69dca4d50958e1c
Shane DeWael <dewaels@purdue.edu>
2016-02-07 16:54:00 -0500

    Votes

--- app.js
+++ app.js
@@ -4,16 +4,24 @@ app.controller('main', [
 	'$scope',
 	function($scope){
 		$scope.links = [
-			{title: 'Link 1', upvotes: 1},
-			{title: 'Link 2', upvotes: 3},
-			{title: 'Link 3', upvotes: 8},
-			{title: 'Link 4', upvotes: 2},
-			{title: 'Link 5', upvotes: 1}
+			{title: 'Link 1', votes: 1},
+			{title: 'Link 2', votes: 3},
+			{title: 'Link 3', votes: 8},
+			{title: 'Link 4', votes: 2},
+			{title: 'Link 5', votes: 1}
 		];
 
 		$scope.addLink = function() {
 			if (!$scope.title) { return; }
-			$scope.links.push({title: $scope.title, upvotes: 0});
+			$scope.links.push({title: $scope.title, votes: 0});
 			$scope.title = '';
+		}
+
+		$scope.incrementVotes = function(link) {
+			link.votes = link.votes + 1;
+		}
+
+		$scope.decrementVotes = function(link) {
+			link.votes = link.votes - 1;
 		}
 }]);
