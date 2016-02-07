34c0e4e3f0279928dcab6152461a88f389af9a81
Shane DeWael <dewaels@purdue.edu>
2016-02-07 17:01:13 -0500

    Add URLs

--- app.js
+++ app.js
@@ -4,17 +4,22 @@ app.controller('main', [
 	'$scope',
 	function($scope){
 		$scope.links = [
-			{title: 'Link 1', votes: 1},
-			{title: 'Link 2', votes: 3},
-			{title: 'Link 3', votes: 8},
-			{title: 'Link 4', votes: 2},
-			{title: 'Link 5', votes: 1}
+			{title: 'Link 1', url: 'http://www.example.com', votes: 1},
+			{title: 'Link 2', url: 'http://www.example.com', votes: 3},
+			{title: 'Link 3', url: 'http://www.example.com', votes: 8},
+			{title: 'Link 4', url: 'http://www.example.com', votes: 2},
+			{title: 'Link 5', url: 'http://www.example.com', votes: 1}
 		];
 
 		$scope.addLink = function() {
-			if (!$scope.title) { return; }
-			$scope.links.push({title: $scope.title, votes: 0});
+			if (!$scope.title || !$scope.url) { return; }
+			$scope.links.push({
+				title: $scope.title,
+				url: $scope.url, 
+				votes: 0
+			});
 			$scope.title = '';
+			$scope.url = '';
 		}
 
 		$scope.incrementVotes = function(link) {
