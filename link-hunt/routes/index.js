b7726ed66f8742d97e8eb1c9eb3e7b227005b3e1
Shane DeWael <dewaels@purdue.edu>
2016-02-17 14:01:19 -0500

    First route

--- link-hunt/routes/index.js
+++ link-hunt/routes/index.js
@@ -1,5 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var Link = mongoose.model('Link');
+
+router.get('/links', function(req, res, next) {
+  Link.find(function(err, links){
+    if(err){ return next(err); }
+
+    res.json(links);
+  });
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
